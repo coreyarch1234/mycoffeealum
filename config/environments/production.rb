1751daf86b06c6d8c8cd4da85a4b1f31d5700c01
coreyarch1234 <harrilal@mit.edu>
2017-01-10 14:14:12 -0800

    changed production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # For devise
+  config.action_mailer.default_url_options = { :host => 'young-taiga-47888.heroku.com' }
+
 end
