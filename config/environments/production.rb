2b9fa864c977321daeca84cd5dfbac67d43dce2d
coreyarch1234 <harrilal@mit.edu>
2017-01-10 19:45:16 -0800

    added port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'https://young-taiga-47888.herokuapp.com' }
   # config.action.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { :host => 'your.websitedomain.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 end
