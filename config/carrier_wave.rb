03f0443a9f137051ddd0f4f8a58ba5a342967e63
coreyarch1234 <harrilal@mit.edu>
2017-01-13 11:42:17 -0800

    added S3 Bucket

---
+++ config/carrier_wave.rb
@@ -0,0 +1,11 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AKIAIANYOHLGZNIVATIQ'],
+      :aws_secret_access_key => ENV['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']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
+end
