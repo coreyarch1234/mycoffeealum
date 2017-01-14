c136ff2643d089479fb7e0a0df70ebf3c250bb11
coreyarch1234 <harrilal@mit.edu>
2017-01-13 22:00:27 -0800

    created .env file

--- config/carrier_wave.rb
+++ config/carrier_wave.rb
@@ -1,13 +1,11 @@
-
-require 'carrierwave'
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => 'AKIAIANYOHLGZNIVATIQ',
-      :aws_secret_access_key => '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'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  'make-mentor-bucket'
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
