3f33545a96399e714a0f68cf0141b9d249ba1d9c
coreyarch1234 <harrilal@mit.edu>
2017-03-02 11:35:47 -0800

    added admin page and connections

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -2,27 +2,47 @@ class ConnectionsController < ApplicationController
   def index
       @users = User.all
       @connections = Connection.all
+      @conversations = Conversation.all
   end
 
   def new
+      @users = User.all
       @connection = Connection.new
   end
 
   def create
-      @connection = Connection.new(connection_params)
-      mentor = User.find(params[:mentee_id])
-      mentee = User.find(params[:mentor_id])
-      connection = Connection.create!(mentee_id: mentee.id, mentor_id: mentor.id)
+    #   @connection = Connection.new(connection_params)
+    #Create a connection between mentor and mentee
+      @mentor = User.find(params[:mentor_id])
+      @mentee = User.find(params[:mentee_id])
+      @connection = Connection.create!(mentee_id: @mentee.id, mentor_id: @mentor.id)
+      if Conversation.between(params[:sender_id],params[:recipient_id]).present?
+          @conversation = Conversation.between(params[:sender_id],
+          params[:recipient_id]).first
+      else
+          @conversation = Conversation.create!(sender_id: @mentee.id, recipient_id: @mentor.id)
+      end
 
-      # create conversation between mentee and mentor
+    #   @conversation = Conversation.create!(sender_id: @mentee.id, recipient_id: @mentor.id)
+      redirect_to conversation_messages_path(@conversation)
 
-      @conversation = Conversation.create({ sender_id: mentor.id, recipient_id: mentee})
-      # message from mentor to mentee
-      @conversation.message.create()
-      # message from mentee to mentor
-      @conversation.message.create()
+      # create conversation between mentee and mentor after connection has been made
 
-      redirect_to users_path
+    #   @conversation = Conversation.create!(sender_id: mentee.id, recipient_id: mentor.id)
+    #   if Conversation.between(params[mentee.id],params[mentor.id]).present?
+    #       @conversation = Conversation.between(params[mentee.id],
+    #       params[mentor.id]).first
+    #   else
+    #       @conversation = Conversation.create(sender_id: mentee.id, recipient_id: mentor.id)
+    #   end
+    #      redirect_to conversation_messages_path(@conversation)
+    #   message from mentee to mentor
+    #   @message = @conversation.messages.new(message_params)
+    #   if @message.save
+    #       redirect_to conversation_messages_path(@conversation)
+    #   end
+
+    #   redirect_to users_path
   end
 
   def destroy
@@ -31,26 +51,15 @@ class ConnectionsController < ApplicationController
 
   private
 
-  #Define the tuple that includes the mentor and mentee id in the connection
-  # class ConnectID < Struct.new(:mentee_id, :mentor_id)
-  #
-  # end
-  # def createTuple(mentee_id, mentor_id)
-  #   @connectArray = []
-  #   @connectID = ConnectID.new mentee_id, mentor_id
-  #   @connectArray.push(connectID)
-  # end
-  # def createSomething
-  #   @connectArray = []
-  #   @connectID = OpenStruct.new
-  #   @connectID[:mentee_id] = 0
-  #   @connectID[:mentee_id] = 0
-  #
-  # end
-
   def connection_params
     params.permit(:mentee_id, :mentor_id)
   end
 
+  # def conversation_params
+  #     params.permit(:sender_id, :recipient_id)
+  # end
 
+  def message_params
+    params.require(:message).permit(:body, :user_id)
+  end
 end
